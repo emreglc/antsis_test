26751df45fb160425d53b2c9573f3ce30007aa25
emreglc <emre.gulec.2002@gmail.com>
2024-03-13 16:17:20 +0300

    design is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
+import axios from 'axios'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [productData, setProductData] = useState(null)
+    const [productNumber, setProductNumber] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setProductData({ "submit": "success" })
+    }
+
+    return (
+        <div className="bg-gray-200 h-screen flex items-center justify-center">
+            <div className="bg-white p-8 rounded-md shadow-md w-[50vw]">
+                {/* String Input */}
+                <form className="mb-4" onSubmit={handleSubmit}>
+                    <label htmlFor="inputText" className="block text-md font-medium text-gray-600">Enter Product Number:</label>
+                    <input value={productNumber} onChange={(e) => setProductNumber(e.target.value)} type="text" id="inputText" name="inputText" className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300" />
+                    <button type='submit' className="mt-2 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Submit</button>
+                </form>
+
+                {/* Display Part */}
+                <div className="border-t pt-4">
+                    <label htmlFor="displayText" className="block text-md font-medium text-gray-600">Product Details</label>
+                    <div id="displayText" className="mt-2 p-4 bg-gray-100 border rounded-md h-96 overflow-auto text-sm">
+                        <pre>{JSON.stringify(productData, null, 2)}</pre>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
